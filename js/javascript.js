b16f5170a565432b287b339e5c9964f0b70e0369
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-03 15:18:19 +0100

    fine es

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,34 @@
-alert(2);
+var nomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
+do {
+
+  var userNome = prompt("dimmi il tuo nome");
+
+} while (!isNaN(userNome));
+
+nomi.push(userNome);
+
+nomi.sort();
+
+for (var i = 0; i < nomi.length; i++) {
+
+  console.log(nomi[i]);
+
+}
+
+var j = 1;
+if ( userNome == nomi[0] ) {
+
+  console.log(1);
+
+} else {
+
+  do {
+
+    ++j;
+
+  } while ( userNome != nomi[j] );
+
+  console.log(j + 1);
+
+}
